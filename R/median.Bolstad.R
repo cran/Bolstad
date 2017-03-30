9fcdb3771246f557acfa22b922a6fae1b8f6b3f3
James Curran <j.curran@auckland.ac.nz>
2017-03-30 10:17:16 +0000

    version 0.2-34

--- R/median.Bolstad.R
+++ R/median.Bolstad.R
@@ -1,13 +1,19 @@
 #' Median generic
 #' 
 #' @param x an object.
-#' @param na.rm Ideally if \code{TRUE} then missing values will be removed, but not currently used
+#' @param na.rm Ideally if \code{TRUE} then missing values will be removed, but not currently used.
+#' @param ... [>=R3.40 only] Not currently used.
 #' @details If \code{x} is an object of class \code{Bolstad} then the posterior 
 #'   median of the parameter of interest will be calculated.
 #' @author James Curran
 #' @method median Bolstad
-#' @export
-median.Bolstad = function(x, na.rm = FALSE){
-  return(quantile(x, probs = 0.5))
+if(is.na(match("...", names(formals(median))))) {
+  median.Bolstad = function(x, na.rm = FALSE) {
+    return(quantile(x, probs = 0.5))
+  }
+}else{
+  median.Bolstad = function(x, na.rm = FALSE, ...) {
+    return(quantile(x, probs = 0.5))
+  }
 }
 
